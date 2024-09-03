b3bbe7c69ad70c1bf33ba9a7dbd30f561d483e81
thay <ugo58956@protonmail.com>
2024-09-03 21:09:41 +0000

    Add more Vector3 methods

--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 #include "Vector3.h"
 
@@ -16,6 +17,10 @@ Vector3 Vector3::operator + (const Vector3 v) const {
   return Vector3(this->x + v.x, this->y + v.y, this->z + v.z);
 }
 
+Vector3 Vector3::operator - (const Vector3 v) const {
+  return *this + (v * -1);
+}
+
 Vector3 Vector3::operator * (const float k) const {
   return Vector3(k * this->x, k * this->y, k * this->z);
 }
@@ -24,6 +29,39 @@ Vector3 Vector3::operator * (const float k) const {
 Vector3 operator * (const float k, Vector3 v) {
   return v*k;
 }
+
+// Static "operators"
+// Multiply two vectors component-wise
+static Vector3 scale(const Vector3 a, const Vector3 b) {
+  return Vector3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+
+// Dot product
+static float dot(const Vector3 a, const Vector3 b) {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+// Cross product
+static Vector3 cross(const Vector3 a, const Vector3 b) {
+  return Vector3(a.y * b.z - a.z * b.y,
+		 a.z * b.x - a.x * b.z,
+		 a.x * b.y - a.y * b.x);
+}
+
+float Vector3::magnitude() const {
+  // We don't use sqrMagnitude for the sake of efficiency
+  return pow(pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2), 0.5);
+}
+
+float Vector3::sqrMagnitude() const {
+  return pow(this->x, 2) + pow(this->y, 2) + pow(this->z, 2);
+}
+
+Vector3 Vector3::normalized() const {
+  float mag = this->magnitude();
+  return mag * *this;
+}
+
 
 void Vector3::print() {
   printf("(%f, %f, %f)\n", x, y, z);
