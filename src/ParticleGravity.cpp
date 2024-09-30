337ca46163a19f9e000264063a2e1387df799e08
Léo Pelazza <leo.pelazza@laposte.net>
2024-09-30 10:51:12 -0400

    Update ParticleGravity.cpp

--- src/ParticleGravity.cpp
+++ src/ParticleGravity.cpp
@@ -2,7 +2,11 @@
 
 
 class ParticleGravity : public ParticleForceGenerator {
-	float gravity = 9.81;
+	Vector3 gravity = Vector3();
+
+	ParticleGravity() {
+		gravity = Vector3(0, -9.81, 0);
+	}
 
 	void UpdateForce(Particle* particle, float duration) {
 		float m = 1 / particle->getInverseMass();
