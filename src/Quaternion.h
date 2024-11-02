f1a6966152b6b7490b002fdb48005fff3043b60b
thay <91734800+fmkr-project@users.noreply.github.com>
2024-11-01 22:27:01 -0400

    Start implementing quaternions

--- src/Quaternion.h
+++ src/Quaternion.h
@@ -1,5 +1,28 @@
 #pragma once
 class Quaternion
 {
+private:
+    float x, y, z, w;
+
+public:
+    Quaternion(float x, float y, float z, float w);
+    ~Quaternion();
+
+    Quaternion static identity;
+    
+    Quaternion operator-(const Quaternion &other) const;
+    Quaternion operator*(const Quaternion &other) const;
+
+    Quaternion operator*(const float &other) const;
+    Quaternion operator/(const float &other) const;
+
+    Quaternion static Neg(const Quaternion &a);
+    Quaternion static Euler(float yaw, float pitch, float roll);
+    float static Magnitude(const Quaternion &a);
+    Quaternion static Conjugate(const Quaternion &a);
+    Quaternion static Inverse(const Quaternion &a);
+    float static Dot(const Quaternion &a, const Quaternion &b);
+    Quaternion static Power(const Quaternion &a, float pow);
+    Quaternion static Slerp(const Quaternion &a, const Quaternion &b, float t);
 };
 
